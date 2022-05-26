45f1aacbf04e25d8ab63686d2e300e4ead01fc0a
Tyler Nijmeh <tylernij@gmail.com>
2022-05-26 11:03:14 -0700

    Add easy run bash script
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+PY_ENTRY="src/main.py"
+PYTHON="python3"
+PYTHONPATH="$SCRIPT_DIR" "$PYTHON" "$SCRIPT_DIR/$PY_ENTRY"
