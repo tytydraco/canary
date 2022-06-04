cc44c2d71ce23703b75893adebcb26ccde7b0744
Tyler Nijmeh <tylernij@gmail.com>
2022-06-04 11:54:01 -0700

    Rewrite in python
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ client/birdhouse.py
@@ -0,0 +1,52 @@
+import pathlib
+
+init_path = pathlib.Path(__file__).parent.resolve()
+reflist_path = f'{init_path}/birdhouse'
+
+
+# Create the reflist file if it does not yet exist
+def prepare_reflist():
+    return
+
+
+# Parse data from the reflist file into an actual list
+def load_reflist():
+    return
+
+
+# Add an entry to the reflist
+def add_reflist():
+    return
+
+
+# Delete an entry from the reflist
+def delete_reflist():
+    return
+
+
+# Display the reflist entries and their online statuses
+def list_reflist():
+    return
+
+
+# Send a command to one entry in the reflist
+def send_com():
+    return
+
+
+# Send a command to all entries in the reflist
+def send_com_all():
+    return
+
+
+# Show usage of the program
+def usage():
+    return
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
